e1a941c10ac36c9b17bb1cd4e4d759fc1e337d3b
wswj9608 <wswj9608@naver.com>
2023-02-15 16:56:58 +0900

    Boilerplate Code 2

--- src/app.ts
+++ src/app.ts
@@ -2,37 +2,35 @@ import express from "express";
 import compression from "compression";  // compresses requests
 import session from "express-session";
 import bodyParser from "body-parser";
-import lusca from "lusca";
-import MongoStore from "connect-mongo";
 import path from "path";
 import bluebird from "bluebird";
-import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
+import dotenv from "dotenv";
+import mysql from "mysql";
+
 
 // Controllers (route handlers)
 import * as homeController from "./controllers/home";
 import * as apiController from "./controllers/api";
+import * as summonerController from "./controllers/summoner";
+import { dbConfig } from "./config/database";
 
 // API keys and Passport configuration
 
 // Create Express server
 const app = express();
 
-// Connect to MongoDB
-
+// Connect to Mysql
+mysql.createConnection(dbConfig);
 
 // Express configuration
-app.set("port", process.env.PORT || 3000);
+dotenv.config();
+
+app.set("port", process.env.EXPRESS_PORT);
 app.set("view engine", "pug");
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({
-    resave: true,
-    saveUninitialized: true,
-    secret: SESSION_SECRET,
-}));
-app.use(lusca.xframe("SAMEORIGIN"));
-app.use(lusca.xssProtection(true));
+
 
 app.use(
     express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
@@ -41,7 +39,8 @@ app.use(
 /**
  * Primary app routes.
  */
-// app.get("/", homeController.index);
+app.get("/", homeController.index);
+app.get("/summoner/:summonerName", summonerController.getSummoner);
 // app.get("/login", userController.getLogin);
 // app.post("/login", userController.postLogin);
 // app.get("/logout", userController.logout);
--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -5,7 +5,8 @@ import { Request, Response } from "express";
  * @route GET /
  */
 export const index = (req: Request, res: Response) => {
-    res.render("home", {
+    console.log(req);
+    res.json({
         title: "Home"
     });
 };
